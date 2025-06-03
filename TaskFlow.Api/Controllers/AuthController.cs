504eb3ab2959bb7c5cc20c8be1d7759f45968222
Yogi Suhagiya <125767544+yogisuhagiya@users.noreply.github.com>
2025-06-03 14:01:39 -0400

    Update AuthController.cs

--- TaskFlow.Api/Controllers/AuthController.cs
+++ TaskFlow.Api/Controllers/AuthController.cs
@@ -1,20 +1,19 @@
-// In TaskFlow.Api/Controllers/AuthController.cs
+
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
-[Route("api/auth")] // Web address will be /api/auth
+[Route("api/auth")]
 public class AuthController : ControllerBase
 {
-    // Simple class to hold login/register info
     public class AuthDetails { public string Username { get; set; } = ""; public string Password { get; set; } = ""; }
 
-    [HttpPost("login")] // For when someone POSTS to /api/auth/login
+    [HttpPost("login")] n
     public IActionResult LoginPlaceholder([FromBody] AuthDetails creds)
     {
         return Ok(new { Message = "Login will go here!" });
     }
 
-    [HttpPost("register")] // For /api/auth/register
+    [HttpPost("register")] 
     public IActionResult RegisterPlaceholder([FromBody] AuthDetails creds)
     {
         return Ok(new { Message = "Registration will go here!" });
