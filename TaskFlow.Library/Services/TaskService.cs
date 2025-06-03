8736ea14380a44db167cd0931f9ddd23ab0d4839
Yogi Suhagiya <125767544+yogisuhagiya@users.noreply.github.com>
2025-06-03 13:58:56 -0400

    Update TaskService.cs

--- TaskFlow.Library/Services/TaskService.cs
+++ TaskFlow.Library/Services/TaskService.cs
@@ -1,10 +1,8 @@
-// In TaskFlow.Library/Services/ITaskService.cs
+
 namespace TaskFlow.Library.Services
 {
     public interface TaskService
     {
-        // Just having the file is enough for now.
-        // Later, you'd add method definitions like:
-        // string GetSomeTaskInfo(int taskId);
+        
     }
 }
