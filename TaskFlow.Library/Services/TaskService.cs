4197bef33f28e43ae3308bd439f10f9119fb9a60
Yogi Suhagiya <yogisuhagiya21@gmail.com>
2025-06-03 12:09:28 -0400

    Add placeholder ITaskService interface

---
+++ TaskFlow.Library/Services/TaskService.cs
@@ -0,0 +1,10 @@
+// In TaskFlow.Library/Services/ITaskService.cs
+namespace TaskFlow.Library.Services
+{
+    public interface TaskService
+    {
+        // Just having the file is enough for now.
+        // Later, you'd add method definitions like:
+        // string GetSomeTaskInfo(int taskId);
+    }
+}
